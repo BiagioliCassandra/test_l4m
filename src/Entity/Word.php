5a45a71c25f19024921346108ff6158363796cee
cassi <biagioli.cassandra@gmail.com>
2019-05-21 16:48:37 +0200

    first commit

---
+++ src/Entity/Word.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\WordRepository")
+ * @UniqueEntity("string")
+ */
+class Word
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\Regex("#^[a-z]{4,12}$#")
+     */
+    private $string;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $creation_date;
+
+    public function __construct() {
+        $this->creation_date = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getString(): ?string
+    {
+        return $this->string;
+    }
+
+    public function setString(string $string): self
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    public function getCreationDate(): ?\DateTimeInterface
+    {
+        return $this->creation_date;
+    }
+
+    public function setCreationDate(\DateTimeInterface $creation_date): self
+    {
+        $this->creation_date = $creation_date;
+
+        return $this;
+    }
+}
